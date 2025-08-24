be9bc6c0395de13016f85f1d7a9f30f803bea208
JJG <jj.garza33@protonmail.com>
2025-08-23 20:00:48 -0600

    added entiry/arch structure to new vhdl file

--- 1-VHDL/newFile.vhd
+++ 1-VHDL/newFile.vhd
@@ -15,3 +15,24 @@
 -- Revision      : address to configuration management site
 --
 --=========================================================================================
+
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
+
+entity <my_module> is
+	   generic(
+		   	
+		   );
+		   port(
+			         
+			   );
+		end <my_module>;
+		
+		architecture rtl of <my_module> is
+			
+			
+			begin
+				
+				
+			end architecture;
